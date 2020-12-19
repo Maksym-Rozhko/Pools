50f9416232222d8d4dde7681e814aea94898d0ac
Maksym <maksymRozhko7@gmail.com>
2020-12-19 02:43:39 +0200

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,170 @@
+const mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+$('.swiper-wrap').on('click', '.slide-item', function() {
+    const index = $(this).data('index');
+    mySwiper.slideTo(index);
+});
+                        
+const slideItem = document.querySelectorAll('.slide-item');
+const mainSlide = document.querySelectorAll('.swiper-slide');
+const prevBtn = document.querySelector('.swiper-button-prev');
+const nextBtn = document.querySelector('.swiper-button-next');
+
+mySwiper.on('slideChange', () => {
+    let currIndexSlide = mySwiper.realIndex;
+
+    switch(currIndexSlide) {
+        case 0:
+            slideItem[0].classList.add('slide-item_active');
+            slideItem[1].classList.remove('slide-item_active');
+            slideItem[2].classList.remove('slide-item_active');
+        break;
+        case 1:
+            slideItem[1].classList.add('slide-item_active');
+            slideItem[0].classList.remove('slide-item_active');
+            slideItem[2].classList.remove('slide-item_active');
+        break;
+        case 2:
+            slideItem[2].classList.add('slide-item_active');
+            slideItem[0].classList.remove('slide-item_active');
+            slideItem[1].classList.remove('slide-item_active');
+        break;
+        default:
+            console.log('slideChange');
+    }
+});
+
+const formBtns = document.querySelectorAll('.form__btn');
+const gallerySubmBtn = document.querySelectorAll('.modal-btn');
+
+const iterAllBtns = (allBtns) => {
+    for (let btn of allBtns) {
+        btn.addEventListener('click', () => {
+            Swal.fire(
+                'Good job!',
+                'Enter your phone number!',
+                'success'
+            );
+            modal.style.display = "none";
+            document.querySelector('body').style.overflow = 'visible';
+        });
+    }
+};
+
+iterAllBtns(formBtns);
+iterAllBtns(gallerySubmBtn);
+
+const headerFixed = document.querySelector('.header-inner');
+const logo = document.getElementById('logo');
+const imgLogo = [
+    './assets/images/header/logo.png',
+    './assets/images/header/logo_fixed.png',
+];
+
+window.onscroll = () => {
+    let scrollY = window.scrollY
+    scrollY > 100 ? headerFixed.classList.add('header-inner__active') : headerFixed.classList.remove("header-inner__active");
+    scrollY > 100 ? logo.src = `${imgLogo[1]}` : logo.src = `${imgLogo[0]}`;
+}
+
+// Gallery 
+let gallerySwiper = new Swiper('.gallery-swiper-container', {
+    spaceBetween: 15,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        565: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 4,
+            slidesPerGroup: 1,
+        },
+        1250: {
+            slidesPerView: 5,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
+let gallerySwiperBottom = new Swiper('.second-gallery-slider', {
+    slidesPerView: 5,
+    spaceBetween: 15,
+    slidesPerGroup: 1,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+        565: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+        768: {
+            slidesPerView: 4,
+            slidesPerGroup: 1,
+        },
+        1250: {
+            slidesPerView: 5,
+            slidesPerGroup: 1,
+        },
+    },
+});
+
+gallerySwiper.controller.control = gallerySwiperBottom;
+gallerySwiperBottom.controller.control = gallerySwiper;
+
+const modalCont = document.querySelector(".modal__box");
+const modal = document.getElementById("galleryModal");
+const modalBtnClose = document.getElementsByClassName("modal__close")[0];
+const swiperSlidePool = document.querySelectorAll('.swiper-slide-pool');
+
+const getSrcImages = (e) => {
+    let target = e.target;
+    let imgSrc = target.src;
+    modalCont.children[0].children[0].setAttribute('src', imgSrc);
+}
+
+modalBtnClose.addEventListener('click', () => {
+    modal.style.display = "none";
+    document.querySelector('body').style.overflow = 'visible';
+});
+
+for (item of swiperSlidePool) {
+    item.addEventListener('click', e => {
+        e.preventDefault();
+        getSrcImages(e);
+        modal.style.display = "block";
+        document.querySelector('body').style.overflow = 'hidden';
+    });
+}
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+        document.querySelector('body').style.overflow = 'visible';
+    }
+});
---
+++ assets/js/formMaskTel.js
@@ -0,0 +1,38 @@
+// $(function(){
+//     $('#formTel_1').mask('+38(000) 000-00-00');
+// });
+
+const inputTelElems = document.querySelectorAll('.form__input');
+window.addEventListener("DOMContentLoaded", () => {
+    [].forEach.call( inputTelElems, function(input) {
+    let keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+380 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i !== -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = "";
+    }
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+  });
+});
